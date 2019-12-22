e89d7eb5b5351c923f688961b048b4e8530af945
aniyagnik <ani789yagnik@gmail.com>
2019-12-22 17:13:23 +0530

    adding data

---
+++ src/components/card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card-component">
+      <h1 class='heading'>{{ object.name }}</h1>
+      <p>
+        {{ object.discription }}
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'object' ],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+  .card-component {
+    border: 1px solid white;
+    padding: 10px;
+    background-color: black;
+    opacity:0.8;
+    margin-bottom:10px;
+  }
+
+  .card-component:hover{
+    opacity: 1;
+    border: 3px solid crimson;
+  }
+
+  .card-component:hover .heading {
+    color: crimson;
+  }
+</style>
--- src/views/SolarSystem.vue
+++ src/views/SolarSystem.vue
@@ -2,12 +2,57 @@
   <div class="solarSystem">
     <br>
     <h1>Our Solar System</h1>
+    <b-container>
+      <b-row>
+        <b-col>
+          <card-component v-bind:object='celestialBodies[0]'></card-component>
+        </b-col>
+        <b-col>
+          <card-component v-bind:object='celestialBodies[1]'></card-component>
+        </b-col>
+        <b-col>
+          <card-component v-bind:object='celestialBodies[2]'></card-component>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <card-component v-bind:object='celestialBodies[3]'></card-component>
+        </b-col>
+        <b-col>
+          <card-component v-bind:object='celestialBodies[4]'></card-component>
+        </b-col>
+        <b-col>
+          <card-component v-bind:object='celestialBodies[5]'></card-component>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <card-component v-bind:object='celestialBodies[6]'></card-component>
+        </b-col>
+        <b-col>
+          <card-component v-bind:object='celestialBodies[7]'></card-component>
+        </b-col>
+        <b-col>
+          <card-component v-bind:object='celestialBodies[8]'></card-component>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
+import cardComponent from '@/components/card.vue'
+import celestialBodies from '@/data/solarSystem.js'
+
 export default {
-  name: 'solarSystem'
+  components: {
+    'card-component': cardComponent
+  },
+  data () {
+    return {
+      celestialBodies
+    }
+  }
 }
 </script>
 
